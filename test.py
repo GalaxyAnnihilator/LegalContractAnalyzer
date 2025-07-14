6f814178b8c841077fadfc7cf2c1cb666758ef00
GalaxyAnnihilator <minhduongqo@gmail.com>
2025-07-14 11:06:40 -0400

    successfully opened an API endpoint for openai calling

--- test.py
+++ test.py
@@ -1,7 +1,23 @@
-from transformers import pipeline
+import os
+from openai import OpenAI
 
-pipe = pipeline("text-generation", model="unsloth/Qwen2.5-0.5B-Instruct-bnb-4bit")
-messages = [
-    {"role": "user", "content": "Who are you?"},
-]
-pipe(messages)
+API_KEY = os.getenv("OPENAI_API_KEY", "sk-fake-for-testing")
+BASE_URL = "https://glowing-workable-arachnid.ngrok-free.app/v1"
+
+client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
+
+stream = client.chat.completions.create(
+    model="Qwen/Qwen3-0.6B",
+    messages=[{"role":"user","content":"Hi, how are you?"}],
+    temperature=0.7,
+    max_tokens=1024,
+    stream=True,
+)
+
+# Consume the stream
+for event in stream:
+    choice = event.choices[0]
+    # If the model is still generating, .delta.content will hold the next token(s)
+    if delta := choice.delta.content:
+        print(delta, end="", flush=True)
+    print("\n")    
