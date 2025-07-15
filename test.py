8dde7bfb282a46876cb99a02807d61849a723377
tmd <minhduongqo@gmail.com>
2025-07-15 22:57:03 +0700

    terribly failed to build app.py :((

--- test.py
+++ test.py
@@ -1,16 +1,27 @@
 import os
 from openai import OpenAI
+from httpx import Timeout
+from dotenv import load_dotenv
 
-API_KEY = os.getenv("OPENAI_API_KEY", "TRANMINHDUONGDEPTRAI")
+# Load variables from .env into environment
+load_dotenv()
+
+API_KEY = os.getenv("OPENAI_API_KEY","TRANMINHDUONGDEPTRAI")
 BASE_URL = "https://glowing-workable-arachnid.ngrok-free.app/v1"
 
-client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
+client = OpenAI(
+    api_key=API_KEY,
+    base_url=BASE_URL,
+    timeout=Timeout(30.0),
+)
+
+print("Succesfully created a client instance")
 
 stream = client.chat.completions.create(
-    model="Qwen/Qwen3-0.6B",
-    messages=[{"role":"user","content":"Hi, how are you?"}],
+    model="Qwen3-0.6B",
+    messages=[{"role":"user","content":"Do you know what RAG technology is?"}],
     temperature=0.7,
-    max_tokens=1024,
+    max_tokens=512,
     stream=True,
 )
 
